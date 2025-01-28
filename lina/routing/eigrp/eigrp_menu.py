edc02fca3296b23a31e58678f9c1a01dbabd7964
Garrett McCollum <gmccollu@cisco.com>
2025-01-28 14:50:28 -0500

    added eigrp running config to both menu and dump

--- lina/routing/eigrp/eigrp_menu.py
+++ lina/routing/eigrp/eigrp_menu.py
@@ -8,17 +8,19 @@ from lina.routing.eigrp.eigrp_topology.eigrp_topology import eigrp_topology
 from lina.routing.eigrp.eigrp_traffic.eigrp_traffic import eigrp_traffic
 from lina.routing.eigrp.eigrp_routing_table.eigrp_routing_table import eigrp_routing_table
 from lina.routing.eigrp.dump_all_eigrp_data.dump_all_eigrp_data import dump_all_eigrp_data
+from lina.routing.eigrp.eigrp_running_config.eigrp_running_config import eigrp_running_config
 
 
 def eigrp_menu():
     menu_options = {
-        "1": ("EIGRP Events", eigrp_events),
-        "2": ("EIGRP Interfaces", eigrp_interfaces),
-        "3": ("EIGRP Neighbors", eigrp_neighbors),
-        "4": ("EIGRP Topology", eigrp_topology),
-        "5": ("EIGRP Traffic", eigrp_traffic),
-        "6": ("EIGRP Routing Table", eigrp_routing_table),
-        "7": ("Dump All EIGRP Data", dump_all_eigrp_data),
+        "1": ("EIGRP Running Configuration", eigrp_running_config),
+        "2": ("EIGRP Events", eigrp_events),
+        "3": ("EIGRP Interfaces", eigrp_interfaces),
+        "4": ("EIGRP Neighbors", eigrp_neighbors),
+        "5": ("EIGRP Topology", eigrp_topology),
+        "6": ("EIGRP Traffic", eigrp_traffic),
+        "7": ("EIGRP Routing Table", eigrp_routing_table),
+        "8": ("Dump All EIGRP Data", dump_all_eigrp_data),
         "0": ("Exit", None),
     }
 
@@ -27,7 +29,7 @@ def eigrp_menu():
         options_display = {key: description for key, (description, _) in menu_options.items()}
         display_formatted_menu("EIGRP Menu", options_display)
 
-        choice = input("Select an option (0-6): ").strip()
+        choice = input("Select an option (0-8): ").strip()
 
         if choice in menu_options:
             description, function = menu_options[choice]
