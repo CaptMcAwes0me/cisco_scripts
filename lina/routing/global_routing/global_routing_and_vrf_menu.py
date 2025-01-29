579b3fc2c56ffb092b872b35910ac0af1d36f844
Garrett McCollum <gmccollu@cisco.com>
2025-01-29 11:52:40 -0500

    added all global routing modules

--- lina/routing/global_routing/global_routing_and_vrf_menu.py
+++
@@ -1,5 +0,0 @@
-def global_menu():
-    print("Accessing global_routing Menu...")
-    # Add your logic for NAT menu here
-    # For example, you can define options for NAT troubleshooting, configuration, etc.
-
