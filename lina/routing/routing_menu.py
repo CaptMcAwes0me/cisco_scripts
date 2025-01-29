2cc94ee416147ca2e630efc5c5bf4db3b803b0ba
Garrett McCollum <gmccollu@cisco.com>
2025-01-28 22:24:55 -0500

    removed isis as FTD doesn't support

--- lina/routing/routing_menu.py
+++ lina/routing/routing_menu.py
@@ -2,7 +2,6 @@
 
 from lina.routing.global_routing.global_menu import global_menu
 from lina.routing.eigrp.eigrp_menu import eigrp_menu
-from lina.routing.isis.isis_menu import isis_menu
 from lina.routing.ospf.ospf_menu import ospf_menu
 from lina.routing.bgp.bgp_menu import bgp_menu
 from lina.routing.rip.rip_menu import rip_menu
@@ -16,9 +15,8 @@ def routing_menu():
         "2": ("EIGRP", eigrp_menu),
         "3": ("OSPF", ospf_menu),
         "4": ("BGP", bgp_menu),
-        "5": ("ISIS", isis_menu),
-        "6": ("RIP", rip_menu),
-        "7": ("VRF", vrf_menu),
+        "5": ("RIP", rip_menu),
+        "6": ("VRF", vrf_menu),
         "0": ("Exit", None),
     }
 
@@ -27,7 +25,7 @@ def routing_menu():
         options_display = {key: description for key, (description, _) in menu_options.items()}
         display_formatted_menu("Routing Menu", options_display)
 
-        choice = input("Select an option (0-7): ").strip()
+        choice = input("Select an option (0-6): ").strip()
 
         if choice in menu_options:
             description, function = menu_options[choice]
