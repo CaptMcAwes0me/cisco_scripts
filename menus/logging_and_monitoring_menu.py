c22b75c43021a678f54db7ee1ec7db30c2edc571
Garrett McCollum <gmccollu@cisco.com>
2025-02-03 21:28:21 -0500

    fixed data dump menu

--- menus/logging_and_monitoring_menu.py
+++ menus/logging_and_monitoring_menu.py
@@ -1,4 +1,4 @@
-def logging_menu():
+def logging_and_monitoring_menu():
     print("Accessing traffic_analysis_and_logging_menu Menu...")
     # Add your logic for NAT menu here
     # For example, you can define options for NAT troubleshooting, configuration, etc.
