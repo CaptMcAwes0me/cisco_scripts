4d2a55354935af5b03ac81898a53cc1c21deaacc
Garrett McCollum <gmccollu@cisco.com>
2025-02-05 21:46:39 -0500

    fixed data_dump_menu.py

--- menus/data_dump_menu.py
+++ menus/data_dump_menu.py
@@ -5,7 +5,7 @@ from lina.nat.dump_all_nat_data.dump_all_nat_data import dump_all_nat_data
 from firepower.device_information.device_information import device_information
 from menus.routing_dump_menu import routing_dump_menu
 from menus.vpn_dump_menu import vpn_dump_menu
-from menus.failover_menu import failover_menu
+from lina.failover.dump_all_failover_data.dump_all_failover_data import dump_all_failover_data
 from menus.logging_and_monitoring_dump_menu import logging_and_monitoring_dump_menu
 from lina.cluster.dump_all_cluster_data.dump_all_cluster_data import dump_all_cluster_data
 from lina.blocks.dump_all_blocks_data.dump_all_blocks_data import dump_all_blocks_data
@@ -21,7 +21,7 @@ def data_dump_menu():
         "3": ("Connectivity and Traffic", dump_all_conn_and_traffic_data),
         "4": ("Routing", routing_dump_menu),
         "5": ("VPN", vpn_dump_menu),
-        "6": ("High Availability (HA) / Failover", failover_menu),
+        "6": ("High Availability (HA) / Failover", dump_all_failover_data),
         "7": ("Logging and Monitoring", logging_and_monitoring_dump_menu),
         "8": ("Clustering", dump_all_cluster_data),
         "9": ("Block Memory", dump_all_blocks_data),
