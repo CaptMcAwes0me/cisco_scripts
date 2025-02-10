b1a59e29f25b55662670b978e38ef07ffa095740
Garrett McCollum <gmccollu@cisco.com>
2025-02-09 22:20:51 -0500

    fixed s2s_menu.py

--- menus/s2s_menu.py
+++ menus/s2s_menu.py
@@ -4,11 +4,10 @@ from lina.vpn.s2s.s2s_config.s2s_config import (
     s2s_ikev2_vti_config,
     s2s_ikev2_policy_based_config
 )
-from core.utils import get_and_parse_cli_output, display_formatted_menu
+from core.utils import display_formatted_menu
 from lina.vpn.s2s.crypto_isakmp_sa_detail.crypto_isakmp_sa_detail import crypto_isakmp_sa_detail
 from lina.vpn.s2s.crypto_ipsec_sa_detail.crypto_ipsec_sa_detail import crypto_ipsec_sa_detail
 from lina.vpn.s2s.s2s_help.s2s_help import s2s_help
-import re
 
 
 def s2s_menu(selected_peers):
@@ -32,29 +31,16 @@ def s2s_menu(selected_peers):
         if choice in menu_options:
             description, function = menu_options[choice]
             if choice == "1":  # Handle Site-to-Site Configuration
-                for ip_address in selected_peers:
-                    # Determine IKE version and type
-                    tunnel_output = get_and_parse_cli_output(f"show running-config tunnel-group {ip_address}")
+                for peer in selected_peers:
+                    ip_address, ike_version, vpn_type = peer  # Unpack the selection
 
-                    ikev1 = bool(re.search(r"ikev1 pre-shared-key", tunnel_output))
-                    ikev2 = bool(re.search(r"ikev2 (remote|local)-authentication pre-shared-key", tunnel_output))
-
-                    vti_output = get_and_parse_cli_output("show running-config interface | begin Tunnel")
-                    interface_sections = re.findall(r"interface (Tunnel\S+)([\s\S]*?)(?=^interface|\Z)", vti_output, re.MULTILINE)
-
-                    matched_interface = None
-                    for interface, config in interface_sections:
-                        if re.search(rf"tunnel destination {re.escape(ip_address)}", config):
-                            matched_interface = interface
-                            break
-
-                    if ikev1 and matched_interface:
+                    if ike_version == 'ikev1' and vpn_type == 'vti':
                         s2s_ikev1_vti_config(ip_address)
-                    elif ikev1:
+                    elif ike_version == 'ikev1' and vpn_type == 'policy':
                         s2s_ikev1_policy_based_config(ip_address)
-                    elif ikev2 and matched_interface:
+                    elif ike_version == 'ikev2' and vpn_type == 'vti':
                         s2s_ikev2_vti_config(ip_address)
-                    elif ikev2:
+                    elif ike_version == 'ikev2' and vpn_type == 'policy':
                         s2s_ikev2_policy_based_config(ip_address)
 
             elif function:
