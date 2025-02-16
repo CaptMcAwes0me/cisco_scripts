0ac1516d3fb7d48ca4cb71c49838f6a4f998d46e
Garrett McCollum <gmccollu@cisco.com>
2025-02-16 10:37:52 -0500

    testing traffic.py

--- lina/connectivity_and_traffic/traffic/traffic.py
+++ lina/connectivity_and_traffic/traffic/traffic.py
@@ -45,53 +45,58 @@ def traffic_calc(output):
         elif "1 minute input rate" in line and interface:
             values = re.findall(r'(\d+) pkts/sec, *(\d+) bytes/sec', line)
             if values:
-                input_rates_1m[interface] = (int(values[0][1]), int(values[0][0]))  # (bps, pps)
+                input_rates_1m[interface] = [int(values[0][1]), int(values[0][0])]  # [bps, pps]
 
         elif "1 minute output rate" in line and interface:
             values = re.findall(r'(\d+) pkts/sec, *(\d+) bytes/sec', line)
             if values:
-                output_rates_1m[interface] = (int(values[0][1]), int(values[0][0]))  # (bps, pps)
+                output_rates_1m[interface] = [int(values[0][1]), int(values[0][0])]  # [bps, pps]
 
         elif "5 minute input rate" in line and interface:
             values = re.findall(r'(\d+) pkts/sec, *(\d+) bytes/sec', line)
             if values:
-                input_rates_5m[interface] = (int(values[0][1]), int(values[0][0]))  # (bps, pps)
+                input_rates_5m[interface] = [int(values[0][1]), int(values[0][0])]  # [bps, pps]
 
         elif "5 minute output rate" in line and interface:
             values = re.findall(r'(\d+) pkts/sec, *(\d+) bytes/sec', line)
             if values:
-                output_rates_5m[interface] = (int(values[0][1]), int(values[0][0]))  # (bps, pps)
+                output_rates_5m[interface] = [int(values[0][1]), int(values[0][0])]  # [bps, pps]
 
     # Detect possible traffic loops
     detect_traffic_loop(input_rates_1m, "input")
     detect_traffic_loop(output_rates_1m, "output")
 
     # Calculate total traffic
-    total_input_1m, total_input_pps_1m = sum(v[0] for v in input_rates_1m.values()), sum(v[1] for v in input_rates_1m.values())
-    total_input_5m, total_input_pps_5m = sum(v[0] for v in input_rates_5m.values()), sum(v[1] for v in input_rates_5m.values())
-    total_output_1m, total_output_pps_1m = sum(v[0] for v in output_rates_1m.values()), sum(v[1] for v in output_rates_1m.values())
-    total_output_5m, total_output_pps_5m = sum(v[0] for v in output_rates_5m.values()), sum(v[1] for v in output_rates_5m.values())
+    total_input_1m = sum(v[0] for v in input_rates_1m.values())
+    total_input_5m = sum(v[0] for v in input_rates_5m.values())
+    total_output_1m = sum(v[0] for v in output_rates_1m.values())
+    total_output_5m = sum(v[0] for v in output_rates_5m.values())
+
+    total_input_pps_1m = sum(v[1] for v in input_rates_1m.values())
+    total_input_pps_5m = sum(v[1] for v in input_rates_5m.values())
+    total_output_pps_1m = sum(v[1] for v in output_rates_1m.values())
+    total_output_pps_5m = sum(v[1] for v in output_rates_5m.values())
 
     # Print tables
     print("\nINPUT TRAFFIC RATES:")
-    traffic_table(["Interface", "1-Min Input (Bps)", "1-Min Input (pps)", "5-Min Input (Bps)", "5-Min Input (pps)"], input_rates_1m, input_rates_5m)
+    traffic_table(["Interface", "1-Min Input (Bps)", "1-Min Input (pps)", "5-Min Input (Bps)", "5-Min Input (pps)"],
+                  [[iface] + input_rates_1m.get(iface, [0, 0]) + input_rates_5m.get(iface, [0, 0]) for iface in input_rates_1m])
 
     print("\nOUTPUT TRAFFIC RATES:")
-    traffic_table(["Interface", "1-Min Output (Bps)", "1-Min Output (pps)", "5-Min Output (Bps)", "5-Min Output (pps)"], output_rates_1m, output_rates_5m)
+    traffic_table(["Interface", "1-Min Output (Bps)", "1-Min Output (pps)", "5-Min Output (Bps)", "5-Min Output (pps)"],
+                  [[iface] + output_rates_1m.get(iface, [0, 0]) + output_rates_5m.get(iface, [0, 0]) for iface in output_rates_1m])
 
     # Print Total Summary
     print("\nTOTAL TRAFFIC SUMMARY:")
-    traffic_table(["Traffic Type", "1-Min Total (Bps)", "1-Min Total (pps)", "5-Min Total (Bps)", "5-Min Total (pps)"], {
-        "Total": (total_input_1m + total_output_1m, total_input_pps_1m + total_output_pps_1m,
-                  total_input_5m + total_output_5m, total_input_pps_5m + total_output_pps_5m)
-    }, {})
+    traffic_table(["Traffic Type", "1-Min Total (Bps)", "1-Min Total (pps)", "5-Min Total (Bps)", "5-Min Total (pps)"],
+                  [["Total", total_input_1m + total_output_1m, total_input_pps_1m + total_output_pps_1m,
+                    total_input_5m + total_output_5m, total_input_pps_5m + total_output_pps_5m]])
 
     # Print Bandwidth Usage
     print("\nTOTAL BANDWIDTH USAGE:")
-    traffic_table(["Traffic Type", "1-Min Bandwidth (Bps)", "5-Min Bandwidth (Bps)"], {
-        "Input": (convert_bps_to_readable(total_input_1m), convert_bps_to_readable(total_input_5m)),
-        "Output": (convert_bps_to_readable(total_output_1m), convert_bps_to_readable(total_output_5m))
-    }, {})
+    traffic_table(["Traffic Type", "1-Min Bandwidth", "5-Min Bandwidth"],
+                  [["Input", convert_bps_to_readable(total_input_1m), convert_bps_to_readable(total_input_5m)],
+                   ["Output", convert_bps_to_readable(total_output_1m), convert_bps_to_readable(total_output_5m)]])
 
     # Print Bandwidth Calculation
     print("\n**Bandwidth Usage Calculation**:")
@@ -100,12 +105,11 @@ def traffic_calc(output):
 
     # Print Average Packet Size
     print("\nAVERAGE PACKET SIZE:")
-    traffic_table(["Traffic Type", "1-Min Avg Packet (bytes)", "5-Min Avg Packet (bytes)"], {
-        "Input": (total_input_1m // total_input_pps_1m if total_input_pps_1m else 0,
-                  total_input_5m // total_input_pps_5m if total_input_pps_5m else 0),
-        "Output": (total_output_1m // total_output_pps_1m if total_output_pps_1m else 0,
-                   total_output_5m // total_output_pps_5m if total_output_pps_5m else 0)
-    }, {})
+    traffic_table(["Traffic Type", "1-Min Avg Packet (bytes)", "5-Min Avg Packet (bytes)"],
+                  [["Input", total_input_1m // total_input_pps_1m if total_input_pps_1m else 0,
+                            total_input_5m // total_input_pps_5m if total_input_pps_5m else 0],
+                   ["Output", total_output_1m // total_output_pps_1m if total_output_pps_1m else 0,
+                             total_output_5m // total_output_pps_5m if total_output_pps_5m else 0]])
 
 
 def detect_traffic_loop(rates, direction):
